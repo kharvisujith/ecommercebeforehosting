375d0fc04aba6b541795de7c31fb26cfbc829add
Prasanth <PRASANTH.BAJANTHRI@Honeywell.com>
2022-05-16 12:51:16 +0530

    Before Hosting

---
+++ Restore/Controllers/ProductsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Restore.Data;
+using Restore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Restore.Extensions;
+using Restore.RequestHelpers;
+using System.Text.Json;
+
+namespace Restore.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : BaseApiController
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<PagedList<Product>>> GetProducts([FromQuery]ProductParams productParams)
+        {
+            //var products = await _context.Products.ToListAsync();
+            // return Ok(products);
+            System.Diagnostics.Debug.WriteLine("getproducts called in backend");
+            System.Diagnostics.Debug.WriteLine(productParams.SearchItem + productParams.PageNumber);
+
+           var query = _context.Products
+                .Sort(productParams.OrderBy)
+                .Search(productParams.SearchItem)
+                .Filter(productParams.Brands, productParams.Types)
+                .AsQueryable();
+
+            //  return await query.ToListAsync(); 
+            var products = await PagedList<Product>.ToPagedList(query, 
+                productParams.PageNumber, productParams.PageSize);
+
+            Response.AddPaginationHeader(products.MetaData);
+
+            return products;
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null) return NotFound();
+
+            return product;
+        }
+
+        [HttpGet("filters")]
+        public async Task<IActionResult> GetFilters()
+        {
+
+            var brands = await _context.Products.Select(p => p.Brand).Distinct().ToListAsync();
+            var types = await _context.Products.Select(p => p.Type).Distinct().ToListAsync();
+
+            return Ok(new { brands, types });
+
+        }
+    }
+}
---
+++ client/src/api/agent.ts
@@ -0,0 +1,148 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+
+
+import "../../node_modules/react-toastify/dist/ReactToastify.css"
+import { toast } from 'react-toastify';
+import { history } from './../index';
+import { store, useAppSelector } from "../store/configureStore";
+import { PaginationResponse } from "../models/pagination";
+
+
+
+//axios.defaults.baseURL = process.env.REACT_APP_API_URL
+
+axios.defaults.baseURL = "http://localhost:16310/api/"
+
+axios.defaults.withCredentials = true
+
+const resBody = (response: AxiosResponse) => response.data
+
+const sleep = () => new Promise(resolve => setTimeout(resolve,500))
+
+axios.interceptors.request.use(config => {
+    const token = store.getState().account.user?.token
+    if(token) config.headers!.Authorization = `Bearer ${token}`
+    return config
+})
+
+
+axios.interceptors.response.use(async response =>{
+  //  if(process.env.NODE_ENV === 'development') await sleep()
+  await sleep()
+        
+
+    const pagination = response.headers['pagination']
+  
+    if(pagination){
+        response.data = new  PaginationResponse(response.data, JSON.parse(pagination))
+        return response
+
+    }
+    return response
+
+
+  
+},(error:AxiosError) => {
+    const {data, status} = error.response as any
+
+    switch(status){
+        case 400: 
+        if(data.errors){
+            const modelStateErrors : string [] = []
+            for(const key in  data.errors){
+                if(data.errors[key]){
+                    modelStateErrors.push(data.errors[key])
+                }       
+            }
+    
+            throw modelStateErrors.flat()
+            
+        }
+        toast.error(data.title)
+        
+        break
+        case 404: 
+        history.push('/not-found')
+        break
+        
+        case 500: 
+        // history.push('/server-error')
+        history.push({
+            pathname: '/server-error',
+            state : {error: data}
+        }
+        )
+        break
+        
+        case 401: toast.error(data.title)
+        break
+
+        default: break
+
+    }
+   // toast.error(data.title)
+
+    return Promise.reject(error.response)
+}
+    )
+
+const request = {
+    // get : (url : string, params?:URLSearchParams) => axios.get(url, {params}).then(resBody),
+    get : (url:string, params?:URLSearchParams) => axios.get(url, {params}).then(resBody),
+    post : (url:string, body:{}) => axios.post(url, body).then(resBody),
+    put : (url:string, body:{}) => axios.put(url, body).then(resBody),
+    delete: (url:string) => axios.delete(url).then(resBody)
+}
+
+const catalog = {
+     list : (params:URLSearchParams)=>  request.get('Products',params),
+    //  list : () => request.get(`Products?OrderBy=name&SerachItem=v&PageNumber=1&PageSize=6`),
+                            
+    details : (id:number) => request.get(`products/${id}`),
+    fetchfilters : ()=> request.get('products/filters')
+}
+
+
+const basket = {
+    get : ()=> request.get('Basket'),
+    addItem : (productId : number, quantity = 1)=> request.post(`basket?productId=${productId}&quantity=${quantity}`, {}),
+    removeItem : (productId : number, quantity = 1)=> request.delete(`basket?productId=${productId}&quantity=${quantity}`)
+}
+
+const testErrors = {
+    get400error: ()=>request.get('buggy/bad-request').catch(error => console.log(error)),
+    get401error: ()=> request.get('buggy/unauthorised').catch(error=>console.log(error)),
+    get404error: ()=> request.get('buggy/not-found').catch(error => console.log(error)),
+    get500error: ()=> request.get('buggy/server-error').catch(error => console.log(error)),
+    getvalidationerror: ()=> request.get('buggy/validation-error')
+}
+
+
+const account = {
+    login: (values:any) => request.post('account/login', values),
+    register: (values:any) => request.post('account/register',values),
+    currentUser : ()=> request.get('account/currentUser'),
+    fetchAddress: ()=> request.get('account/saveAddress')
+}
+
+const orders ={
+    list: ()=> request.get('orders'),
+    fetch : (id:number) => request.get(`orders${id}`),
+    create : (values:any) => request.post('orders', values),
+   
+}
+
+const payment= {
+    createPaymentIntent : ()=> request.post('payment', {})
+}
+
+const agent = {
+    catalog,
+    testErrors,
+    basket,
+    account,
+    orders,
+    payment
+}
+
+export default agent;
